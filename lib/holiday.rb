422bf1e5891a148f28003103f87ca6b9bd4f8c2e
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-08-10 01:42:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,7 +18,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-return holiday_hash[:spring][:fourth_of_july] = "BBQ"
+binding.pry 
+return holiday_hash[:spring][:fourth_of_july] 
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
